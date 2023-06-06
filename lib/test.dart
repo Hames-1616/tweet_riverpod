825649ac9f2046953c419542d27be2be8374f6f2
Hames-1616 <bilquis748@gmail.com>
2023-06-06 20:37:51 +0530

    fixed

--- lib/test.dart
+++ lib/test.dart
@@ -12,18 +12,21 @@ class desgin extends StatefulWidget {
 }
 
 class _desginState extends State<desgin> {
+  bool s = true;
+  Icon ikon = Icon(Icons.add_box_outlined);
+  Icon k = Icon(Icons.add_box_rounded);
+
   @override
   Widget build(BuildContext context) {
-    Icon i  =  Icon(Icons.add_box_outlined);
-    Icon k = Icon(Icons.add_box_rounded);
-    Icon ikon = i;
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back)),
+        leading:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
         actions: [
           Container(
-            padding:EdgeInsets.symmetric(horizontal: 10),
-            child: IconButton(onPressed: (){}, icon: const Icon(Icons.share)))
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child:
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.share)))
         ],
       ),
       body: Column(
@@ -32,28 +35,54 @@ class _desginState extends State<desgin> {
           Row(
             children: [
               Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                margin: const EdgeInsets.only(right: 50),
-                width: 200,
-                height: 250,
-                child: Image.asset("assets/svgs/tweet.png")),
-
-                 const Column(
-                  children: [
-                    Text("Category",style: TextStyle(color: Colors.white54),),
-                    SizedBox(height: 5,),    
-                    Text("Indoors",style: TextStyle(color: Colors.white),),
-                    SizedBox(height: 10,),
-                    Text("Size",style: TextStyle(color: Colors.white54),),
-                    SizedBox(height: 5,),    
-                    Text("Small",style: TextStyle(color: Colors.white),),
-                    SizedBox(height: 10,),
-                    Text("Plant",style: TextStyle(color: Colors.white54),),
-                    SizedBox(height: 5,),    
-                    Text("Tweeter",style: TextStyle(color: Colors.white),),
-
-                  ],
-                )
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(right: 50),
+                  width: 200,
+                  height: 250,
+                  child: Image.asset("assets/svgs/tweet.png")),
+              const Column(
+                children: [
+                  Text(
+                    "Category",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Indoors",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Size",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Small",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Plant",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Tweeter",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              )
             ],
           ),
           Container(
@@ -63,68 +92,79 @@ class _desginState extends State<desgin> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text("Tweeter",style: TextStyle(fontSize: 20),),
-                    IconButton(onPressed: (){
-                      setState(() {
-                        ikon =Icon(Icons.add_box_rounded) ;
-                        print("called");
-                      });
-                    }, icon: ikon)
+                    const Text(
+                      "Tweeter",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    s
+                        ? IconButton(
+                            onPressed: () {
+                              setState(() {
+                                s = !s;
+                                print("called");
+                              });
+                            },
+                            icon: ikon)
+                        : IconButton(
+                            onPressed: () {
+                              setState(() {
+                                s = !s;
+                              });
+                            },
+                            icon: k)
                   ],
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(
+                  height: 10,
+                ),
                 const Row(
                   children: [
                     SizedBox(
-                      width: 350,
-                      child: Text("hey there how are you hope you are doing well I'm sorry, but I'm unable to provide direct links to copyrighted materials such as PDFs of books. Sharing or distributing copyrighted content without proper authorization would be against OpenAI's use case policy. However, you can consider checking with your local library or educational institutions as they may have access to digital copies of the book through their subscriptions or databases. Additionally, you can explore online platforms and retailers that offer legal e-book purchases or rentals.Remember, it is important to respect the intellectual property rights of authors and publishers by obtaining books through legitimate means."))
+                        width: 350,
+                        child: Text(
+                            "hey there how are you hope you are doing well I'm sorry, but I'm unable to provide direct links to copyrighted materials such as PDFs of books. Sharing or distributing copyrighted content without proper authorization would be against OpenAI's use case policy. However, you can consider checking with your local library or educational institutions as they may have access to digital copies of the book through their subscriptions or databases. Additionally, you can explore online platforms and retailers that offer legal e-book purchases or rentals.Remember, it is important to respect the intellectual property rights of authors and publishers by obtaining books through legitimate means."))
                   ],
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Column(
                       children: [
-                        IconButton(onPressed: (){}, icon: const Icon(Icons.sunny)),
-                        const Text(
-                         "SunLight"
-                        )
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.sunny)),
+                        const Text("SunLight")
                       ],
                     ),
                     Column(
                       children: [
-                        IconButton(onPressed: (){}, icon: const Icon(Icons.water)),
-                        const Text(
-                         "1L/month"
-                        ),
-                        
-                      ],
-                      
-                    ),
-                    Column(
-                      children: [
-                        IconButton(onPressed: (){}, icon: const Icon(Icons.light)),
-                        const Text(
-                         "29C - 40C"
-                        )
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.water)),
+                        const Text("1L/month"),
                       ],
                     ),
                     Column(
                       children: [
-                        IconButton(onPressed: (){}, icon: const Icon(Icons.place)),
-                        const Text(
-                         "30cm - 40cm"
-                        )
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.light)),
+                        const Text("29C - 40C")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.place)),
+                        const Text("30cm - 40cm")
                       ],
                     )
                   ],
-
                 )
               ],
             ),
           )
-          ],
+        ],
       ),
     );
   }
