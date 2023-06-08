b4f9db62aaaeeb93acfdf87aa4365d750a8594bf
Hames-1616 <bilquis748@gmail.com>
2023-06-08 22:29:04 +0530

    push

--- lib/models/tweet_model.dart
+++ lib/models/tweet_model.dart
@@ -1,8 +1,137 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import '../core/enums/tweet_type_enum.dart';
+
 class Tweet {
   final String text;
   final List<String> hastags;
   final String link;
   final List<String> imageLinks; //appwrite bucket links for the image stored
   final String uid;
-  final String 
+  final TweetType tweetType;
+  final DateTime tweetedAt;
+  final List<String> likes;
+  final List<String> commentIds;
+  final String id; //id of the tweet
+  final int reshareCount;
+  Tweet({
+    required this.text,
+    required this.hastags,
+    required this.link,
+    required this.imageLinks,
+    required this.uid,
+    required this.tweetType,
+    required this.tweetedAt,
+    required this.likes,
+    required this.commentIds,
+    required this.id,
+    required this.reshareCount,
+  });
+
+  Tweet copyWith({
+    String? text,
+    List<String>? hastags,
+    String? link,
+    List<String>? imageLinks,
+    String? uid,
+    TweetType? tweetType,
+    DateTime? tweetedAt,
+    List<String>? likes,
+    List<String>? commentIds,
+    String? id,
+    int? reshareCount,
+  }) {
+    return Tweet(
+      text: text ?? this.text,
+      hastags: hastags ?? this.hastags,
+      link: link ?? this.link,
+      imageLinks: imageLinks ?? this.imageLinks,
+      uid: uid ?? this.uid,
+      tweetType: tweetType ?? this.tweetType,
+      tweetedAt: tweetedAt ?? this.tweetedAt,
+      likes: likes ?? this.likes,
+      commentIds: commentIds ?? this.commentIds,
+      id: id ?? this.id,
+      reshareCount: reshareCount ?? this.reshareCount,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'text': text});
+    result.addAll({'hastags': hastags});
+    result.addAll({'link': link});
+    result.addAll({'imageLinks': imageLinks});
+    result.addAll({'uid': uid});
+    result.addAll({'tweetType': tweetType.type});
+    result.addAll({'tweetedAt': tweetedAt.millisecondsSinceEpoch});
+    result.addAll({'likes': likes});
+    result.addAll({'commentIds': commentIds});
+    result.addAll({'id': id});
+    result.addAll({'reshareCount': reshareCount});
+  
+    return result;
+  }
+
+  factory Tweet.fromMap(Map<String, dynamic> map) {
+    return Tweet(
+      text: map['text'] ?? '',
+      hastags: List<String>.from(map['hastags']),
+      link: map['link'] ?? '',
+      imageLinks: List<String>.from(map['imageLinks']),
+      uid: map['uid'] ?? '',
+      tweetType: TweetType.fromMap(map['tweetType']),
+      tweetedAt: DateTime.fromMillisecondsSinceEpoch(map['tweetedAt']),
+      likes: List<String>.from(map['likes']),
+      commentIds: List<String>.from(map['commentIds']),
+      id: map['id'] ?? '',
+      reshareCount: map['reshareCount']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Tweet.fromJson(String source) => Tweet.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Tweet(text: $text, hastags: $hastags, link: $link, imageLinks: $imageLinks, uid: $uid, tweetType: $tweetType, tweetedAt: $tweetedAt, likes: $likes, commentIds: $commentIds, id: $id, reshareCount: $reshareCount)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Tweet &&
+      other.text == text &&
+      listEquals(other.hastags, hastags) &&
+      other.link == link &&
+      listEquals(other.imageLinks, imageLinks) &&
+      other.uid == uid &&
+      other.tweetType == tweetType &&
+      other.tweetedAt == tweetedAt &&
+      listEquals(other.likes, likes) &&
+      listEquals(other.commentIds, commentIds) &&
+      other.id == id &&
+      other.reshareCount == reshareCount;
+  }
+
+  @override
+  int get hashCode {
+    return text.hashCode ^
+      hastags.hashCode ^
+      link.hashCode ^
+      imageLinks.hashCode ^
+      uid.hashCode ^
+      tweetType.hashCode ^
+      tweetedAt.hashCode ^
+      likes.hashCode ^
+      commentIds.hashCode ^
+      id.hashCode ^
+      reshareCount.hashCode;
+  }
 }
